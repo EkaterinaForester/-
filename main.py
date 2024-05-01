d459403d7d8f4697c7328424d0e185f6fd07f47f
EkaterinaForester <168631916+EkaterinaForester@users.noreply.github.com>
2024-05-01 16:53:38 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+nam='Habib Nurmagometov'
+print(nam[6])
+print(nam[:7])
+name=' Lesnichenko'
+print(name[0])
+print(name[-1])
+print(name[2:4])
+print(name[::-1])
+print(len(name))
+print(nam + name)
